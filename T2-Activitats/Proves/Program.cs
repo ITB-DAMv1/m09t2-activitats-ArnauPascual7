f6afd36f723f5ef4d4e069de8252828a1c5f9986
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-04-30 13:12:26 +0200

    Async Task

---
+++ T2-Activitats/Proves/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Proves
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+
+            Task<int> task = Move(cts.Token, '$');
+
+            Console.ReadKey();
+
+            cts.Cancel();
+
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            Console.WriteLine("Espais recorreguts: " + task.Result);
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static async Task<int> Move(CancellationToken tk, char ch)
+        {
+            string spacer = " ";
+            int count = 0;
+
+            try
+            {
+                while (!tk.IsCancellationRequested)
+                {
+                    Console.Clear();
+                    Console.WriteLine(spacer + ch);
+
+                    await Task.Delay(500, tk);
+
+                    spacer += " ";
+                    count++;
+                }
+            }
+            catch (Exception)
+            {
+                return count;
+            }
+            return count;
+        }
+    }
+}
